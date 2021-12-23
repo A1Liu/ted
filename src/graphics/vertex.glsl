73413e41e0b3b0416fab655234c42dfb0b34101e
Albert Liu <albertymliu@gmail.com>
2021-12-23 00:37:48 -0500

    removed integer divions and modulo from the vertex shader

--- src/graphics/vertex.glsl
+++ src/graphics/vertex.glsl
@@ -22,25 +22,27 @@ const vec2 c_pos_offset = vec2(-1.0, 1.0);
 2,4     5
 */
 
-// TODO remove integer division/modulo I guess?
+// This was a lot simpler when I could use integer division. :((
 void main() {
     // Translation from vertex ID to text coordinates
-    int block_index = gl_VertexID % 6;
-    int point_index = gl_VertexID / 6;
+    float vertex_id = float(gl_VertexID);
+    float block_index = floor(mod(vertex_id, 6.));
+    float point_index = floor(vertex_id / 6.);
 
-    int is_first = int(block_index < 3);
-    int is_right = int(block_index % 2);
-    int is_bot = int((block_index + is_first * 2) >= 4);
-    uvec2 block_offset = uvec2(is_right, is_bot);
+    float is_first = float(block_index < 3.);
+    float is_right = floor(mod(block_index, 2.));
+    float is_bot = float((block_index + is_first * 2.) >= 4.);
+    vec2 block_offset = vec2(is_right, is_bot);
 
-    int line_width = int(u_dims.x);
-    int point_x = point_index % line_width;
-    int point_y = point_index / line_width;
+    // Floating point precision means that removing these 0.001 results in weird
+    // stuff. I don't understand why.
+    float point_x = floor(mod(point_index + 0.001, u_dims.x));
+    float point_y = floor(point_index / u_dims.x + 0.001);
 
-    uvec2 in_pos = uvec2(point_x, point_y) + block_offset;
+    vec2 in_pos = vec2(point_x, point_y) + block_offset;
 
     // Translation from text coordinates to clip space
-    vec2 pos = vec2(in_pos) / u_dims * 2.0;
+    vec2 pos = in_pos / u_dims * 2.0;
     pos = pos * c_pos_transform + c_pos_offset;
 
     gl_Position = vec4(pos, 0.0, 1.0);
