bfbcdfd6f1973f58d230c12e136f39043f2da821
Albert Liu <albertymliu@gmail.com>
2021-12-23 00:41:44 -0500

    removed some dumbass constants

--- src/graphics/vertex.glsl
+++ src/graphics/vertex.glsl
@@ -11,9 +11,6 @@ uniform vec2 u_atlas_dims;
 out vec2 v_glyph_pos;
 flat out uint v_block_type;
 
-const vec2 c_pos_transform = vec2(1.0, -1.0);
-const vec2 c_pos_offset = vec2(-1.0, 1.0);
-
 /*
 0       1,3
 
@@ -43,7 +40,7 @@ void main() {
 
     // Translation from text coordinates to clip space
     vec2 pos = in_pos / u_dims * 2.0;
-    pos = pos * c_pos_transform + c_pos_offset;
+    pos = pos * vec2(1.0, -1.0) + vec2(-1.0, 1.0);
 
     gl_Position = vec4(pos, 0.0, 1.0);
     v_glyph_pos = vec2(in_glyph_pos) / vec2(u_atlas_dims);
