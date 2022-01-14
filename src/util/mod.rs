2a21b5b348d64b8ff92eb25581c907c31c30e93f
Albert Liu <albertymliu@gmail.com>
2022-01-14 01:11:09 -0500

    refactoring a bit

---
+++ src/util/mod.rs
@@ -0,0 +1,46 @@
+pub use mint::*;
+pub use winit::event_loop::EventLoopProxy;
+pub use winit::window::Window;
+
+#[macro_use]
+#[cfg(target_arch = "wasm32")]
+pub mod wasm;
+
+mod pod;
+
+#[cfg(target_arch = "wasm32")]
+pub use wasm::*;
+
+pub use pod::*;
+
+#[cfg(debug_assertions)]
+pub fn expect<V, E>(res: Result<V, E>) -> V
+where
+    E: core::fmt::Debug,
+{
+    return res.unwrap();
+}
+
+#[cfg(not(debug_assertions))]
+pub fn expect<V, E>(res: Result<V, E>) -> V {
+    let err = match res {
+        Ok(v) => return v,
+        Err(err) => err,
+    };
+
+    panic!("Expected value");
+}
+
+pub fn unwrap<V>(opt: Option<V>) -> V {
+    if let Some(v) = opt {
+        return v;
+    }
+
+    panic!("Expected value");
+}
+
+pub type Rect = Vector2<u32>;
+
+pub const fn new_rect(x: u32, y: u32) -> Rect {
+    return Vector2 { x, y };
+}
--- src/util/pod.rs
+++ src/util/pod.rs
@@ -1,157 +1,10 @@
+use super::{expect, unwrap};
 use crate::alloc_api::*;
 use alloc::alloc::Layout;
 use core::num::NonZeroUsize;
 use core::ops::*;
 use core::ptr::NonNull;
-pub use mint::*;
-pub use winit::event_loop::EventLoopProxy;
-pub use winit::window::Window;
 
-#[cfg(target_arch = "wasm32")]
-pub use wasm::*;
-
-#[macro_use]
-#[cfg(target_arch = "wasm32")]
-pub mod wasm {
-    use super::*;
-
-    pub use wasm_bindgen::prelude::*;
-    pub use wasm_bindgen::JsCast;
-
-    pub type JsFunc = dyn 'static + FnMut() -> Result<(), JsValue>;
-
-    pub fn enclose(f: impl 'static + FnMut() -> Result<(), JsValue>) -> Closure<JsFunc> {
-        return Closure::wrap(Box::new(f) as Box<JsFunc>);
-    }
-
-    pub fn get_canvas() -> Result<web_sys::HtmlCanvasElement, JsValue> {
-        let window = unwrap(web_sys::window());
-        let document = unwrap(window.document());
-        let canvas = unwrap(document.get_element_by_id("canvas"));
-        let canvas: web_sys::HtmlCanvasElement = canvas.dyn_into::<web_sys::HtmlCanvasElement>()?;
-        return Ok(canvas);
-    }
-
-    #[macro_export]
-    macro_rules! out {
-        ($str:expr, $( $e:expr ),+ ) => {{
-            #[cfg(debug_assertions)]
-            {
-                out!(@CLEAN, core::concat!("[{}:{}]: ", $str, "\n"), file!(), line!(), $( $e ),+ );
-            }
-        }};
-        (@CLEAN, $str:expr, $( $e:expr ),+ ) => {{
-            let s = format!( $str, $( $e ),+ );
-            $crate::util::wasm::console_log(&s);
-        }};
-    }
-
-    #[macro_export]
-    macro_rules! dbg {
-        ($fmt:literal) => {{
-             out!("{}", $fmt);
-        }};
-        ($fmt:literal, $( $e:expr ),+ ) => {{
-             out!($fmt, $( $e ),+ );
-        }};
-        ($expr:expr) => {{
-            out!("{} = {:?}", stringify!($expr), $expr);
-        }};
-        () => {{
-            out!("{}", "Nothing to see here");
-        }};
-    }
-
-    #[macro_export]
-    macro_rules! println {
-        ($fmt:literal) => {{
-             out!("{}", $fmt);
-        }};
-        ($fmt:literal, $( $e:expr ),+ ) => {{
-             out!($fmt, $( $e ),+ );
-        }};
-        ($expr:expr) => {{
-             out!("{} = {:?}", stringify!($expr), $expr);
-        }};
-        () => {{
-            out!("{}", "Nothing to see here");
-        }};
-    }
-
-    #[macro_export]
-    macro_rules! print {
-        ( $( $arg:tt )* ) => {{
-            println!( $( $arg )* );
-        }};
-    }
-
-    #[macro_export]
-    macro_rules! panic {
-        ( $( $arg:tt )* ) => {{
-            println!( $( $arg )* );
-            #[cfg(debug_assertions)]
-            core::panic!();
-
-            #[cfg(not(debug_assertions))]
-            core::arch::wasm32::unreachable();
-        }};
-    }
-
-    #[macro_export]
-    macro_rules! unreachable {
-        ( $( $arg:tt )* ) => {{
-            panic!()
-        }};
-    }
-
-    pub fn console_log(a: &str) {
-        log(a);
-    }
-
-    #[wasm_bindgen]
-    extern "C" {
-        #[wasm_bindgen(js_namespace = console)]
-        fn log(a: &str);
-    }
-}
-
-#[cfg(debug_assertions)]
-pub fn expect<V, E>(res: Result<V, E>) -> V
-where
-    E: core::fmt::Debug,
-{
-    return res.unwrap();
-}
-
-#[cfg(not(debug_assertions))]
-pub fn expect<V, E>(res: Result<V, E>) -> V {
-    let err = match res {
-        Ok(v) => return v,
-        Err(err) => err,
-    };
-
-    panic!("Expected value");
-}
-
-pub fn unwrap<V>(opt: Option<V>) -> V {
-    if let Some(v) = opt {
-        return v;
-    }
-
-    panic!("Expected value");
-}
-
-pub type Rect = Vector2<u32>;
-
-pub const fn new_rect(x: u32, y: u32) -> Rect {
-    return Vector2 { x, y };
-}
-
-// ----------------------------------------------------------------------------
-//
-//                                  POD ARRAY
-//
-// ----------------------------------------------------------------------------
 struct DataInfo {
     size: usize,
     align: usize,
@@ -233,17 +86,13 @@ where
     where
         R: RangeBounds<usize>,
         I: IntoIterator<Item = T>,
-        T: core::fmt::Debug,
     {
         let range = self.raw.translate_range(range);
         let mut iter = values.into_iter();
         self._splice(range, &mut iter);
     }
 
-    fn _splice(&mut self, range: Range<usize>, mut values: &mut dyn Iterator<Item = T>)
-    where
-        T: core::fmt::Debug,
-    {
+    fn _splice(&mut self, range: Range<usize>, mut values: &mut dyn Iterator<Item = T>) {
         let (start, end) = (range.start, range.end);
 
         if !self.raw.range_is_valid(start, end) {
