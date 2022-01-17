8a6ad4293917624f58d220424424bbff4b6331bc
Albert Liu <albertymliu@gmail.com>
2022-01-17 02:38:05 -0500

    rewrote the code a lil to be simpler

--- src/gon.rs
+++ src/gon.rs
@@ -40,8 +40,61 @@ fn slice_token<'a>(string: &'a [u8]) -> Token<'a> {
     return Token::Str(unsafe { core::str::from_utf8_unchecked(string) });
 }
 
-fn string_token<'a>(string: &[u8]) -> Token<'a> {
-    return Token::String(unsafe { core::str::from_utf8_unchecked(string).to_string() });
+fn parse_string<'a>(bytes: &'a [u8], temp_string: &mut Pod<u8>) -> (usize, Token<'a>) {
+    let mut index = 0;
+    let mut is_escape = false;
+
+    while let Some(&b) = bytes.get(index) {
+        if b == b'\\' {
+            is_escape = true;
+            break;
+        }
+
+        if b == b'"' {
+            break;
+        }
+
+        index += 1;
+    }
+
+    let done_so_far = &bytes[0..index];
+    index += 1;
+
+    if !is_escape {
+        return (index, slice_token(done_so_far));
+    }
+
+    temp_string.clear();
+    temp_string.reserve(done_so_far.len());
+
+    for &byte in done_so_far {
+        temp_string.push(byte);
+    }
+
+    while let Some(&b) = bytes.get(index) {
+        index += 1;
+
+        if is_escape {
+            // TODO support more escapes I guess? Right now this
+            // supports \n \' \" and a few other things.
+            //                  - Albert Liu, Jan 17, 2022 Mon 02:17 EST
+            match b {
+                b'n' => temp_string.push(b'\n'),
+
+                _ => temp_string.push(b),
+            }
+        }
+
+        match b {
+            b'"' => break,
+            b'\\' => is_escape = true,
+            _ => temp_string.push(b),
+        }
+    }
+
+    let value = unsafe { core::str::from_utf8_unchecked(&*temp_string).to_string() };
+
+    return (index, Token::String(value));
 }
 
 // fn string_token<'a>(string: Cow<'a, [u8]>) -> Token<'a> {
@@ -87,61 +140,11 @@ fn tokenize<'a>(data: &'a str) -> Vec<Token<'a>> {
             }
 
             index += 1;
+            let (parsed_len, tok) = parse_string(&bytes[index..], &mut current_string);
 
-            let begin = index;
-            let mut is_escape = false;
+            tokens.push(tok);
+            index += parsed_len;
 
-            while let Some(&b) = bytes.get(index) {
-                if b == b'\\' {
-                    is_escape = true;
-                    break;
-                }
-
-                if b == b'"' {
-                    break;
-                }
-
-                index += 1;
-            }
-
-            let done_so_far = &bytes[begin..index];
-            index += 1;
-
-            if !is_escape {
-                tokens.push(slice_token(done_so_far));
-                continue;
-            }
-
-            current_string.reserve(done_so_far.len());
-            for &byte in done_so_far {
-                current_string.push(byte);
-            }
-
-            while let Some(&b) = bytes.get(index) {
-                if is_escape {
-                    // TODO support more escapes I guess? Right now this
-                    // supports \n \' \" and a few other things.
-                    //                  - Albert Liu, Jan 17, 2022 Mon 02:17 EST
-                    match b {
-                        b'n' => current_string.push(b'\n'),
-
-                        _ => current_string.push(b),
-                    }
-                }
-
-                match b {
-                    b'"' => break,
-                    b'\\' => is_escape = true,
-                    _ => current_string.push(b),
-                }
-
-                index += 1;
-            }
-
-            tokens.push(string_token(&*current_string));
-            index += 1;
-
-            current_string.clear();
             continue;
         }
 
