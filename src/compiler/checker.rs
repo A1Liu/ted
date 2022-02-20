8ebb0740254baae24bd5fd3199b8cce96ac1b7c9
Albert Liu <albertymliu@gmail.com>
2022-02-19 21:11:17 -0500

    did sum'n

--- src/compiler/checker.rs
+++ src/compiler/checker.rs
@@ -4,111 +4,87 @@ use crate::compiler::types::*;
 use crate::util::*;
 use std::collections::hash_map::HashMap;
 
-pub fn check_ast(ast: &Ast) -> Result<Pod<Op>, Error> {
-    let mut ops = Pod::new();
+#[derive(Debug, PartialEq, Clone, Copy)]
+pub enum Type {
+    // Means that the expression that returns this value doesn't ever return
+    // a value directly (early return, loop forever, crash, ...)
+    Never,
+
+    // Void in C
+    Null,
+
+    Unsigned,
+    String,
+}
+
+pub fn check_ast(ast: &Ast) -> Result<(), Error> {
     let mut env = TypeEnv {
-        next_id: 0,
+        type_of: HashMap::new(),
         scope: HashMap::new(),
     };
 
-    env.check_block(&ast.block, &mut ops)?;
+    env.check_block(&ast.block)?;
 
-    return Ok(ops);
-}
-
-#[derive(Clone, Copy)]
-struct VarInfo {
-    value: OpResult,
+    return Ok(());
 }
 
 struct TypeEnv {
-    next_id: u32,
-    scope: HashMap<u32, VarInfo>,
+    type_of: HashMap<*const Expr, Type>,
+    scope: HashMap<u32, &'static Expr>,
 }
 
 impl TypeEnv {
-    fn id(&mut self) -> u32 {
-        let id = self.next_id;
-        self.next_id += 1;
-        return id;
-    }
+    fn check_block(&mut self, block: &Block) -> Result<Type, Error> {
+        let mut ty = Type::Null;
 
-    fn check_block(&mut self, block: &Block, ops: &mut Pod<Op>) -> Result<(), Error> {
         for expr in block.stmts {
-            self.check_expr(expr, ops)?;
+            ty = self.check_expr(expr)?;
         }
 
-        return Ok(());
+        return Ok(ty);
     }
 
-    fn check_expr(&mut self, expr: &Expr, ops: &mut Pod<Op>) -> Result<OpResult, Error> {
+    fn check_expr(&mut self, expr: &'static Expr) -> Result<Type, Error> {
         use ExprKind::*;
 
-        let id = self.id();
-        let mut op = Op {
-            kind: OpKind::Null { id },
-            loc: expr.loc,
-        };
+        let mut ty;
 
         match expr.kind {
             Integer(value) => {
-                op.kind = OpKind::Unsigned { id, value };
-                ops.push(op);
-
-                return Ok(op.kind.result());
+                ty = Type::Unsigned;
             }
 
             Let { symbol, value } => {
-                let result = self.check_expr(value, ops)?;
+                let result = self.check_expr(value)?;
 
-                let info = VarInfo { value: result };
-
-                if let Some(prev) = self.scope.insert(symbol, info) {
-                    return Err(Error::new("redeclared variable", op.loc));
+                if let Some(prev) = self.scope.insert(symbol, expr) {
+                    return Err(Error::new("redeclared variable", expr.loc));
                 }
 
-                return Ok(OpResult::Null);
+                ty = Type::Null;
             }
 
             BinaryOp { kind, left, right } => {
-                let left = self.check_expr(left, ops)?;
-                let right = self.check_expr(right, ops)?;
+                let left_ty = self.check_expr(left)?;
+                let right_ty = self.check_expr(right)?;
 
-                let ty = left.ty();
-                if ty != right.ty() {
+                if left_ty != right_ty {
                     return Err(Error::new(
                         "binary operation should be with values that are the same type",
                         expr.loc,
                     ));
                 }
 
-                if kind == BinaryExprKind::Add {
-                    let result = OpResult::Value { id, ty };
-
-                    op.kind = OpKind::Add {
-                        result,
-                        left,
-                        right,
-                    };
-
-                    ops.push(op);
-
-                    return Ok(result);
-                }
-
-                unreachable!();
+                ty = left_ty;
             }
 
             _ => unreachable!(),
         }
-    }
 
-    fn check_binary(
-        &mut self,
-        op: BinaryExprKind,
-        left: &Expr,
-        right: &Expr,
-    ) -> Result<OpResult, Error> {
-        return Ok(OpResult::Null);
+        if let Some(_) = self.type_of.insert(expr, ty) {
+            panic!("idk");
+        }
+
+        return Ok(ty);
     }
 }
--- src/compiler/parser.rs
+++ src/compiler/parser.rs
@@ -636,7 +636,7 @@ const OPERATORS: [OperatorInfo; 256] = {
     info
 };
 
-fn lex(table: &mut StringTable, file: u32, s: &str) -> Result<Pod<Token>, Error> {
+pub fn lex(table: &mut StringTable, file: u32, s: &str) -> Result<Pod<Token>, Error> {
     let mut tokens = Pod::new();
     let bytes = s.as_bytes();
 
@@ -909,61 +909,5 @@ impl StringTable {
         self.names.push(s);
 
         return id;
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use crate::util::*;
-    use core::fmt::Write;
-
-    // eventually use src/bin
-
-    #[test]
-    fn test_parser() {
-        let mut table = StringTable::new();
-        let mut files = FileDb::new();
-
-        let text = r#"
-        let a = 12
-        let b = a + 12 + 13
-        let c = print(a,b,)
-        print(a, b)
-        "#;
-
-        if let Err(e) = files.add("data.liu", text) {
-            panic!("{}", e);
-        }
-
-        let data = match lex(&mut table, 0, text) {
-            Ok(data) => data,
-            Err(e) => {
-                let mut out = String::new();
-
-                expect(e.render(&files, &mut out));
-
-                eprintln!("{}\n", out);
-                panic!("{:?}", e);
-            }
-        };
-
-        let ast = match parse(&table, 0, data) {
-            Ok(data) => data,
-            Err(e) => {
-                let mut out = String::new();
-
-                expect(e.render(&files, &mut out));
-
-                eprintln!("{}\n", out);
-                panic!("{:?}", e);
-            }
-        };
-
-        let printed = format!("{:#?}", ast.block);
-
-        println!("{}", printed);
-
-        // panic!("viewing");
     }
 }
