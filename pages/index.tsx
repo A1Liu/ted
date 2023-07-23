5d462fa2eb478b916ab281175087bd09c1179668
Albert Liu <albertymliu@gmail.com>
2023-07-23 14:58:55 -0700

    meh

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,12 @@ import React, { useRef } from "react";
 import ReactDOM from "react-dom";
 // import Editor from "@monaco-editor/react";
 
+function Ted() {
+  const editorRef = useRef(null);
+
+  return <canvas ref={editorRef} height={500} width={1400}></canvas>;
+}
+
 export default function Index() {
   const editorRef = useRef(null);
 
@@ -14,16 +20,15 @@ export default function Index() {
   }
 
   return (
-    <div>
+    <div
+      style={{
+        width: "100vw",
+        height: "100vh",
+        backgroundImage: "url('./bg-image.jpg');",
+      }}
+    >
       <button onClick={showValue}>Show value</button>
-      {/*
-      <Editor
-        height="90vh"
-        defaultLanguage="javascript"
-        defaultValue="// some comment"
-        onMount={handleEditorDidMount}
-      />
-      */}
+      <Ted />
     </div>
   );
 }
